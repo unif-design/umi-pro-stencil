3e6b1922072f82ec32d8e4f863ecb2c14e77cac6
qq382724925 <382724935@qq.com>
2026-06-17 22:39:20 +0800

    feat: wire utoopack tailwind v4 and react-query umi plugins

--- .umirc.ts
+++ .umirc.ts
@@ -1,36 +1,24 @@
 import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
-  antd: {},
+  antd: {
+    appConfig: {},
+  },
   access: {},
   model: {},
   initialState: {},
   request: {},
+  reactQuery: {},
+  tailwindcss: {},
   layout: {
     title: '@umijs/max',
   },
   routes: [
-    {
-      path: '/',
-      redirect: '/home',
-    },
-    {
-      name: '首页',
-      path: '/home',
-      component: './Home',
-    },
-    {
-      name: '权限演示',
-      path: '/access',
-      component: './Access',
-    },
-    {
-      name: ' CRUD 示例',
-      path: '/table',
-      component: './Table',
-    },
+    { path: '/', redirect: '/home' },
+    { name: '首页', path: '/home', component: './Home' },
+    { name: '权限演示', path: '/access', component: './Access' },
+    { name: ' CRUD 示例', path: '/table', component: './Table' },
   ],
   npmClient: 'pnpm',
   utoopack: {},
 });
-
